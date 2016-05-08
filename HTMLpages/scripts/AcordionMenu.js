3b04732fea9f0e4125efe97dc00a5e939d7a5029
VadimTeslya <TeslyaVadim2@gmail.com>
2016-05-09 00:13:40 +0300

    Add HTML pages

---
+++ HTMLpages/scripts/AcordionMenu.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    $('#menuButton > ul > li:has(ul)').addClass("has-sub");
+
+    $('#menuButton > ul > li > a').click(function () {
+
+        var checkElement = $(this).next();
+
+        $('#menuButton li').removeClass('active');
+        $(this).closest('li').addClass('active');
+
+        if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+            $(this).closest('li').removeClass('active');
+            checkElement.slideUp('normal');
+        }
+
+        if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+            $('#menuButton ul ul:visible').slideUp('normal');
+            checkElement.slideDown('normal');
+        }
+
+        if (checkElement.is('ul')) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+});
